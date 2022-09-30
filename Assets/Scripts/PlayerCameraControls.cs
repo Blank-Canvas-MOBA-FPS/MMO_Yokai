fb12b36fef3fd635bc6babdcb19134379bfbe39e
TheyCallMePie <aidog100doesgaming@gmail.com>
2022-09-30 00:18:44 -0400

    80 percent player controller

---
+++ Assets/Scripts/PlayerCameraControls.cs
@@ -0,0 +1,24 @@
+using Cinemachine;
+using UnityEngine;
+using Mirror;
+
+public class PlayerCameraControls : NetworkBehaviour
+{
+    [Header("Camera")]
+    [SerializeField] private Vector2 maxFollowOffset = new Vector2(-1f, 6f);
+    [SerializeField] private Vector2 cameraVelocity = new Vector2(4f, 0.25f);
+    [SerializeField] private Transform playerTransform = null;
+    [SerializeField] private CinemachineVirtualCamera virtualCamera = null;
+
+    private PlayerCameraControls controls;
+    private CinemachineTransposer transposer;
+
+    public override void OnStartAuthority()
+    {
+        transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+
+        virtualCamera.gameObject.SetActive(true);
+
+        enabled = true;
+    }
+}
